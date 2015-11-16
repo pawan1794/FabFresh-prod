993d792b12be9185b56be98d81282eb7d18e18a6
h2ri <ha219ri@gmail.com>
2015-11-16 16:51:55 +0530

    static fix2

--- FabFresh/settings.py
+++ FabFresh/settings.py
@@ -129,15 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/static'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_ROOT = os.path.join(
-                  os.path.dirname(
-                      os.path.dirname(
-                          os.path.abspath(__file__)
-                      )
-                  ),
-                  'static'
-              )
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 SOCIAL_AUTH_ENABLED_BACKENDS=('facebook')
