a6b67b3403f3a82cbd2dc7691c2c436fbf9f2493
hari <ha219ri@gmail.com>
2015-12-08 15:15:56 +0530

    user model change

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import UserInfo
+from models import UserInfo, UserProfile
 # Register your models here.
 admin.site.register(UserInfo)#, OrderAdmin)
-
+admin.site.register(UserProfile)
