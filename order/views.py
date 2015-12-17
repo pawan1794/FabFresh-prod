2fa6e086cea2a5552897fa3dd75f12846fcdd770
hari <ha219ri@gmail.com>
2015-12-17 13:46:22 +0530

    FabFresh config

--- order/views.py
+++ order/views.py
@@ -22,7 +22,6 @@ import datetime
 from django.utils.timezone import utc
 from random import randint
 import math
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 def message(self, phone ,message):
     url1 = "http://bhashsms.com/api/sendmsg.php?user=7204680605&pass=9ba84c5&sender=Ffresh&phone="+phone+"&text="+message+"&priority=ndnd&stype=normal"
