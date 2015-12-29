c67ae1cc55ee095af3ee2aa758d6d666d1b39fd2
hari <ha219ri@gmail.com>
2015-12-29 21:15:36 +0530

    celery

--- FabFresh/task.py
+++ FabFresh/task.py
@@ -1,6 +1,6 @@
-import os
+#import os
 #from celery import Celery
-from django.conf import settings
+#from django.conf import settings
 
 #app = Celery('fabfresh', broker='amqp://hari:hari@52.48.75.63:5672/fabfresh')
 #app = Celery('fabfresh',broker='amqp://guest@localhost//')
