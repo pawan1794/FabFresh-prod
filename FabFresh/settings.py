701f7ba629ba8981e2fe6cb0bb864aaff5f18964
hari <ha219ri@gmail.com>
2015-12-30 11:14:34 +0530

    statc file

--- FabFresh/settings.py
+++ FabFresh/settings.py
@@ -132,9 +132,9 @@ PUSH_NOTIFICATIONS_SETTINGS = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
-STATIC_ROOT = "/opt/python/current/app/"
+STATIC_URL = '/static/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
+#STATIC_ROOT = "/opt/python/current/app/FabFresh/FabFresh/static"
 
 
 SOCIAL_AUTH_ENABLED_BACKENDS=('facebook')
