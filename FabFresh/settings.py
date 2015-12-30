2407d6bf49ee727c8b86c7c3a89dd3d72081c8eb
hari <ha219ri@gmail.com>
2015-12-30 12:08:29 +0530

    static site mapping

--- FabFresh/settings.py
+++ FabFresh/settings.py
@@ -136,7 +136,7 @@ PUSH_NOTIFICATIONS_SETTINGS = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/static/'
+STATIC_URL = '/opt/python/current/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..','static') 
 #STATIC_ROOT = "/opt/python/current/app/FabFresh/FabFresh/static"
 
